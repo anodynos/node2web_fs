40e39c2033952276667bc87ace7e8e94897c6594
anodynos <agelos.pikoulas@gmail.com>
2014-02-02 23:14:33 +0200

    browserify version '3.24.10'

--- fs.js
+++ fs.js
@@ -1,10 +1,9 @@
-/** 'fs' nodejs core module browserify-ied with `--standalone fs`.
-Supports all module systems (commonjs, AMD & `window.fs`) - check browserify docs.
+/** 'fs' nodejs core module browserify-ied with `--standalone fs`. Should support all module systems (commonjs, AMD & `window.fs`) - check browserify docs.
 
 From [node2web](http://github.com/anodynos/node2web) collection,
 should/will be exposed as 'fs' to [bower](http://bower.io) for *browser* usage.
 
-browserify version: '3.24.10' 
+browserify version: '3.24.10', build date 'Sun Feb 02 2014 23:14:33 GMT+0200 (EET)' 
 **/
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.fs=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
--- readme.md
+++ readme.md
@@ -1,7 +1,6 @@
-'fs' nodejs core module browserify-ied with `--standalone fs`.
-Supports all module systems (commonjs, AMD & `window.fs`) - check browserify docs.
+'fs' nodejs core module browserify-ied with `--standalone fs`. Should support all module systems (commonjs, AMD & `window.fs`) - check browserify docs.
 
 From [node2web](http://github.com/anodynos/node2web) collection,
 should/will be exposed as 'fs' to [bower](http://bower.io) for *browser* usage.
 
-browserify version: '3.24.10'
+browserify version: '3.24.10', build date 'Sun Feb 02 2014 23:14:33 GMT+0200 (EET)'
